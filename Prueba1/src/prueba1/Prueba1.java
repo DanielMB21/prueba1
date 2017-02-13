71d2d202d3283d89d67ec50746124d975ceb79f9
Alumno <Alumno@U005-10.itver.edu.mx>
2017-02-13 15:52:59 -0600

    Proyecto base

---
+++ Prueba1/src/prueba1/Prueba1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba1;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Prueba1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    JFrame ventana1;
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame ventana1 = new JFrame("Ventana de pruba1");
+        ventana1.setVisible(true);
+    }
+    
+}
